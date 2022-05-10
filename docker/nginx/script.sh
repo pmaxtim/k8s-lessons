50f954c938b3098cb857d0b6dd5339bb3311e14e
mtimoshchenko <mtimoshchenko@imprivata.com>
2022-05-10 18:27:30 +0400

    CC-0000 [MASTER] - Lesson-5 fixed external secrets logic

--- docker/nginx/script.sh
+++ docker/nginx/script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cat /etc/hostname >> /usr/share/nginx/html/index.html
-echo  "<h3><font color=gold>The first secret: $THE_FIRST_SECRET </h3>" >> /usr/share/nginx/html/index.html
-echo -e "<h3><font color=gold>The second secret: $THE_SECOND_SECRET </h3>" >> /usr/share/nginx/html/index.html
+echo  "<h3><font color=gold>The secret DNS: $NGINX_EXTERNAL_DNS </h3>" >> /usr/share/nginx/html/index.html
+echo -e "<h3><font color=gold>The secret username: $NGINX_USERNAME </h3>" >> /usr/share/nginx/html/index.html
+echo -e "<h3><font color=gold>The secret password: $NGINX_PASSWORD </h3>" >> /usr/share/nginx/html/index.html
 
 nginx -g 'daemon off;'
