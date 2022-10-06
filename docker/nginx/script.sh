39d08f576e14234854ae86673563c56d3e0e6161
mtimoshchenko <mtimoshchenko@imprivata.com>
2022-10-06 18:06:39 +0400

    chore: changed nginx page colors

--- docker/nginx/script.sh
+++ docker/nginx/script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cat /etc/hostname >> /usr/share/nginx/html/index.html
-echo  "<h3><font color=gold>The secret DNS: $NGINX_EXTERNAL_DNS </h3>" >> /usr/share/nginx/html/index.html
-echo -e "<h3><font color=gold>The secret username: $NGINX_USERNAME </h3>" >> /usr/share/nginx/html/index.html
-echo -e "<h3><font color=gold>The secret password: $NGINX_PASSWORD </h3>" >> /usr/share/nginx/html/index.html
+echo  "<h3><font color=green>The secret DNS: $NGINX_EXTERNAL_DNS </h3>" >> /usr/share/nginx/html/index.html
+echo -e "<h3><font color=blue>The secret username: $NGINX_USERNAME </h3>" >> /usr/share/nginx/html/index.html
+echo -e "<h3><font color=red>The secret password: $NGINX_PASSWORD </h3>" >> /usr/share/nginx/html/index.html
 
 nginx -g 'daemon off;'
